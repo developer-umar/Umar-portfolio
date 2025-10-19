14b327c998acce8022058158bcbae48aafe0767e
Mohammad Umar <mohammadumar8010@gmail.com>
2025-10-19 21:58:06 +0530

    final fix

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,4 +1,4 @@
-import { env } from '@/env.mjs';
+// import { env } from '@/env.mjs';
 
 export const siteConfig = {
   title:
@@ -63,5 +63,5 @@ export const siteConfig = {
     'Available for Hire',
     'Open Source Contributor',
   ],
-  url: env.SITE_URL || 'https://umar-portfolio-teal.vercel.app'
+  url: 'https://umar-portfolio-teal.vercel.app'
 };
