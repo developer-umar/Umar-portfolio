8e84de2076579e9b8b006c4858391f1afb6da858
Mohammad Umar <mohammadumar8010@gmail.com>
2025-10-19 23:12:24 +0530

    fixing import issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import { PropsWithChildren } from 'react';
 import { ActiveSectionProvider } from '@/components/active-section-provider';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/toaster';
-import { fonts } from '@/lib/fonts';
 import { cn } from '@/lib/utils';
+import { fonts } from '@/lib/fonts';
 
 const RootLayout = ({ children }: PropsWithChildren) => {
   const siteUrl = 'https://umar-portfolio-teal.vercel.app';
